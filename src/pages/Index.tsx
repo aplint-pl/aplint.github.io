d758516c399421f5ebbfc7d518817fb2e41ed242
Matt Kutyba <mateusz.kutyba@gmail.com>
2025-07-27 22:52:40 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -13,7 +13,6 @@ const Index = () => {
       <ServicesSection />
       <TeamSection />
       <ProcessSection />
-      <PortfolioSection />
       <TechnologiesSection />
       <ContactSection />
     </div>
