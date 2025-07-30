f43ea75d19e3b1fed89bc22922fa5e723932fae5
Kamil Purzynski <kamil.purzynski@gmail.com>
2025-07-30 16:16:46 +0200

    Set base path for deployment in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/aplint.github.io/',
   server: {
     host: "::",
     port: 8080,
