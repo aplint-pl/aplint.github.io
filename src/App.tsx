5f2e283ddb3341a199a684a5334d8a093b54cbc9
Mateusz Kutyba <mateusz.kutyba@gmail.com>
2026-02-16 22:59:28 +0100

    Update matomo container URL

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
       const _mtm = window._mtm = window._mtm || [];
       _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
       const d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-      g.async=true; g.src='https://matomo.aplint.pl/js/container_UWactGaS.js'; s.parentNode.insertBefore(g,s);
+      g.async=true; g.src='https://matomo.aplint.pl/js/container_bHu0xCzw.js'; s.parentNode.insertBefore(g,s);
   }, [])
 
   return (
